a13a0d173adda3c881f5d97c5cdfad77e772f3c1
Piotr Hryniewicz <piotr@medcase.health>
2022-05-31 07:51:18 +0200

    fix: Fix hex values with hash and increase version

--- colors_accessibility/color.py
+++ colors_accessibility/color.py
@@ -52,7 +52,7 @@ class Color:
             self.color_values = ''.join(self.color_values)
         color_value = self.color_values.replace('#', '')
         if len(color_value) == 3:
-            self.color_values = f"#{''.join([character * 2 for character in self.color_values])}"
+            self.color_values = f'#{color_value[0] * 2}{color_value[1] * 2}{color_value[2] * 2}'
 
     @processing_registry.register('rgb')
     def process_rgb(self):
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="colors_accessibility",
-    version="0.0.2",
+    version="0.0.3",
     author="Piotr Hryniewicz",
     author_email="phryniewicz.dev@gmail.com",
     description="Package to process and change colors between color spaces and to tweak input colors to meet WCAG 2.1 "
